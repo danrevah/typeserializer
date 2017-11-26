5c5c169dfd7d02e20380e8a7a6db9efbadf7754e
drevah <drevah@outbrain.com>
2017-11-26 12:37:51 +0200

    Add more tests for coverage

---
+++ src/helpers.spec.ts
@@ -0,0 +1,15 @@
+
+import {expect} from 'chai';
+import 'reflect-metadata';
+import 'mocha';
+import {isObject} from './helpers';
+
+describe('Helpers', () => {
+
+  it('should check isObject', () => {
+    expect(isObject({})).to.be.true;
+    expect(isObject([])).to.be.false;
+    expect(isObject(42)).to.be.false;
+  });
+
+});
