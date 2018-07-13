1a6c03a3a3aeef6fd1ace6b3e35b98572adc5b0d
danrevah <danrevah89@gmail.com>
2018-07-13 17:10:15 +0300

    playing

--- src/PropertiesDecorators/Expose.spec.ts
+++ src/PropertiesDecorators/Expose.spec.ts
@@ -7,12 +7,12 @@ import {serialize} from '../Serializer/Serializer';
 class Foo {
 
   @Expose()
-  prop = 'prop';
+  prop: any;
 
-  prop2 = 'prop2';
+  prop2: any;
 
   @Expose()
-  prop3 = 'prop3';
+  prop3: any;
 }
 
 describe('Expose', () => {
@@ -20,6 +20,11 @@ describe('Expose', () => {
   it('should add metadata to the object expose list with the specific property', () => {
     const foo = new Foo();
 
+    foo.prop = 'prop';
+    foo.prop2 = 'prop2';
+    foo.prop3 = 'prop3';
+
+    console.log('yo',Object.getOwnPropertyDescriptor(foo, 'prop'));
 
     expect(serialize(foo)).to.equal('{"prop2":"prop2"}');
   });
