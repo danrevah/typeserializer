48baccfdcb1914ee6c62c5b6d1ee696f4b7ddc31
danrevah <danrevah89@gmail.com>
2018-07-13 17:14:41 +0300

    Expose decorator

--- src/PropertiesDecorators/Expose.ts
+++ src/PropertiesDecorators/Expose.ts
@@ -1,13 +1,8 @@
 import {ExposeSymbol} from '../consts';
 
-export function Expose() {
+export function Expose(fn?: Function) {
   return function<T> (target: T, key: keyof T) {
-    const metadata = Reflect.getMetadata(ExposeSymbol, target);
-
-    if (!metadag)
-    Reflect.defineMetadata(ExposeSymbol, true, target);
-
-    console.log('metadata',metadata);
-
+    const list = Reflect.getMetadata(ExposeSymbol, target) || [];
+    Reflect.defineMetadata(ExposeSymbol, list.concat({key, fn}), target);
   };
 }
