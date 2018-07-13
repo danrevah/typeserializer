51e046b146bd454b84415b8ddd34182edc556e0e
danrevah <danrevah89@gmail.com>
2018-07-13 18:14:28 +0300

    Expose hierarchy

--- src/PropertiesDecorators/Expose.spec.ts
+++ src/PropertiesDecorators/Expose.spec.ts
@@ -6,7 +6,7 @@ import {serialize} from '../Serializer/Serializer';
 
 class Foo {
 
-  @Expose({ name: 'bar' })
+  @Expose({ name: 'foo' })
   prop: string = 'prop';
 
   prop2: string = 'prop2';
@@ -14,11 +14,50 @@ class Foo {
   prop3: string = 'prop3';
 }
 
+class Bar {
+
+  @Expose({ name: 'bar' })
+  prop: string = 'prop';
+
+  prop2: string = 'prop2';
+
+  prop3: string = 'prop3';
+
+  fn() {
+    this.prop2 = 'prop2-2';
+  }
+
+  fn2() {
+    this.prop2 = 'prop2-2';
+  }
+}
+
+class TwoLevels {
+
+  @Expose({name: 'barClass'})
+  bar: Bar;
+
+  constructor() {
+    this.bar = new Bar();
+    this.bar.prop = 'prop-changed';
+  }
+}
+
 describe('Expose', () => {
 
   it('should expose properties while serializing', () => {
     const foo = new Foo();
-    expect(serialize(foo)).to.equal('{"bar":"prop","prop2":"prop2","prop3":"prop3"}');
+    expect(serialize(foo)).to.equal('{"foo":"prop","prop2":"prop2","prop3":"prop3"}');
+  });
+
+  it('should expose properties while serializing class with functions', () => {
+    const bar = new Bar();
+    expect(serialize(bar)).to.equal('{"bar":"prop","prop2":"prop2","prop3":"prop3"}');
+  });
+
+  it('should expose properties while serializing class with hierarchy', () => {
+    const twoLevels = new TwoLevels();
+    expect(serialize(twoLevels)).to.equal('{"barClass":{"prop":"prop-changed","prop2":"prop2","prop3":"prop3"}}');
   });
 
 });
