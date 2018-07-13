dd30e66acd0b72bc8afd8d2c4d580dd3a81049f6
danrevah <danrevah89@gmail.com>
2018-07-13 18:09:27 +0300

    Exclusion

--- src/Serializer/Serializer.ts
+++ src/Serializer/Serializer.ts
@@ -1,17 +1,20 @@
-import {ExcludeSymbol} from '../consts';
+import {ExcludeSymbol, ExposeSymbol} from '../consts';
 
-
-export function serialize(obj: any) {
+export function serialize(obj: any, groups?: string[]) {
   const excludeList = Reflect.getMetadata(ExcludeSymbol, obj) || [];
   const excludeMap = excludeList.reduce((obj: any, { key, config }: any) => (obj[key] = config, obj), {});
+  const exposeList = Reflect.getMetadata(ExposeSymbol, obj) || [];
+  const exposeMap = exposeList.reduce((obj: any, { key, config }: any) => (obj[key] = config, obj), {});
 
-  return Object.getOwnPropertyNames(obj).reduce((json: any, key: string) => {
-    const name = excludeMap[key] && excludeMap[key].name ? excludeMap[key].name : key;
+  return JSON.stringify(
+    Object.getOwnPropertyNames(obj).reduce((json: any, key: string) => {
+      const name = exposeMap[key] && exposeMap[key].name ? exposeMap[key].name : key;
 
-    if (!excludeMap.hasOwnProperty(key)) {
-      json[name] = obj[key];
-    }
+      if (!excludeMap.hasOwnProperty(key)) {
+        json[name] = obj[key];
+      }
 
-    return json;
-  }, {});
+      return json;
+    }, {})
+  );
 }
