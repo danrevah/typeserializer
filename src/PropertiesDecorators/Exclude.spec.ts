f5abc4dd964dd13786090ed51b6bd6104641a696
danrevah <danrevah89@gmail.com>
2018-07-13 17:51:08 +0300

    Exclusion

--- src/PropertiesDecorators/Exclude.spec.ts
+++ src/PropertiesDecorators/Exclude.spec.ts
@@ -15,17 +15,6 @@ class Foo {
   prop3: string = 'prop3';
 }
 
-class Bar {
-
-  @Exclude(() => true)
-  prop: string = 'prop';
-
-  prop2: string = 'prop2';
-
-  @Exclude(() => false)
-  prop3: string = 'prop3';
-}
-
 describe('Exclude', () => {
 
   it('should exclude properties while serializing', () => {
@@ -36,17 +25,6 @@ describe('Exclude', () => {
     expect(serialized.hasOwnProperty('prop2')).to.be.true;
     expect(serialized.hasOwnProperty('prop3')).to.be.false;
     expect(serialized.prop2).to.equal('prop2');
-  });
-
-  it('should exclude properties who match the function while serializing', () => {
-    const bar = new Bar();
-    const serialized = serialize(bar);
-
-    expect(serialized.hasOwnProperty('prop')).to.be.false;
-    expect(serialized.hasOwnProperty('prop2')).to.be.true;
-    expect(serialized.hasOwnProperty('prop3')).to.be.true;
-    expect(serialized.prop2).to.equal('prop2');
-    expect(serialized.prop3).to.equal('prop3');
   });
 
 });
--- src/PropertiesDecorators/Exclude.ts
+++ src/PropertiesDecorators/Exclude.ts
@@ -1,8 +1,9 @@
 import {ExcludeSymbol} from '../consts';
+import {PropertyConfig} from './interfaces';
 
-export function Exclude(fn?: Function) {
+export function Exclude(config?: PropertyConfig) {
   return function<T> (target: T, key: keyof T) {
     const list = Reflect.getMetadata(ExcludeSymbol, target) || [];
-    Reflect.defineMetadata(ExcludeSymbol, list.concat({key, fn}), target);
+    Reflect.defineMetadata(ExcludeSymbol, list.concat({key, config}), target);
   };
 }
