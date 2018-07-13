a454388df1c689822158f22cc51dfe656069a02d
danrevah <danrevah89@gmail.com>
2018-07-13 17:21:07 +0300

    playing

--- src/PropertiesDecorators/Expose.ts
+++
@@ -1,8 +0,0 @@
-import {ExposeSymbol} from '../consts';
-
-export function Expose(fn?: Function) {
-  return function<T> (target: T, key: keyof T) {
-    const list = Reflect.getMetadata(ExposeSymbol, target) || [];
-    Reflect.defineMetadata(ExposeSymbol, list.concat({key, fn}), target);
-  };
-}
