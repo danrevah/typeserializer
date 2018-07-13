33d01954656aa1dd83a77856e546a207ec282958
danrevah <danrevah89@gmail.com>
2018-07-13 19:37:15 +0300

    Todos

--- src/PropertiesDecorators/Exclude.ts
+++ src/PropertiesDecorators/Exclude.ts
@@ -3,6 +3,6 @@ import {ExcludeSymbol} from '../consts';
 export function Exclude() {
   return function<T> (target: T, key: keyof T) {
     const obj = Reflect.getMetadata(ExcludeSymbol, target) || {};
-    Reflect.defineMetadata(ExcludeSymbol, {...obj, ...{[key]: null}}, target);
+    Reflect.defineMetadata(ExcludeSymbol, {...obj, ...{[key]: true}}, target);
   };
 }
