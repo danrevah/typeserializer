3a887c26d7a754e20aa0a21669f61fc3a5256dd6
danrevah <danrevah89@gmail.com>
2018-07-14 21:00:48 +0300

    changes

--- commit.sh
+++
@@ -1,7 +0,0 @@
-
-
-git filter-branch -f --env-filter \
-    'if [ $GIT_COMMIT = de464ea0c3984beae0eb2eba31f3057f0f7185fe ]
-     then
-         export GIT_AUTHOR_DATE="Tue May 15 09:08:33 2018 -0800"
-     fi'
