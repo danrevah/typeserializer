2464aba55bb1a1e46d55a42853f878978eac0041
danrevah <danrevah89@gmail.com>
2018-07-14 21:32:05 +0300

    Expose all

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 
 export {Exclude} from './PropertiesDecorators/Exclude';
+export {Expose} from './PropertiesDecorators/Expose';
+export {After} from './PropertiesDecorators/After';
+export {Before} from './PropertiesDecorators/Before';
+export {Groups} from './PropertiesDecorators/Groups';
+export {Name} from './PropertiesDecorators/Name';
