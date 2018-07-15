c407a93560b2bba52f6440ca947101d8fd94121b
danrevah <danrevah89@gmail.com>
2018-07-15 19:02:23 +0300

    Coverage helpers

--- src/helpers.spec.ts
+++ src/helpers.spec.ts
@@ -1,10 +1,18 @@
 import { expect } from 'chai';
 import 'reflect-metadata';
 import 'mocha';
-import { isObject, versionCompare } from './helpers';
+import {isObject, isUndefined, versionCompare} from './helpers';
 
 describe('Helpers', () => {
+  it('should check isUndefined', () => {
+    let foo;
+    expect(isUndefined(1)).to.be.false;
+    expect(isUndefined(undefined)).to.be.true;
+    expect(isUndefined(foo)).to.be.true;
+  });
+
   it('should check isObject', () => {
+    expect(isObject(null)).to.be.false;
     expect(isObject({})).to.be.true;
     expect(isObject([])).to.be.false;
     expect(isObject(42)).to.be.false;
