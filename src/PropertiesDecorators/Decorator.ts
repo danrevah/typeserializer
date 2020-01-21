56a8f217a701d2336ce2a443224534e888cbfc2c
Dan Revah <danrevah@google.com>
2020-01-21 11:59:41 +0200

    fix(proj): update typesscript

--- src/PropertiesDecorators/Decorator.ts
+++ src/PropertiesDecorators/Decorator.ts
@@ -1,4 +1,4 @@
-import { isUndefined } from "../helpers";
+import { isUndefined } from '../helpers';
 
 export function createDecorator(name: string, keySymbol: Symbol, value: any) {
   return function<T>(target: T, key: keyof T) {
@@ -6,7 +6,7 @@ export function createDecorator(name: string, keySymbol: Symbol, value: any) {
 
     if (!isUndefined(obj[key])) {
       throw new Error(
-        `Cannot apply @${name} decorator twice on property '${key}' of class '${target.constructor.name}'.`
+        `Cannot apply @${name} decorator twice on property '${key}' of class '${(target as any).constructor.name}'.`
       );
     }
 
